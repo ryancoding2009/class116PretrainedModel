b55dcfffd3f2a7ec2c0a1e181aac37f4134a9cc1
ryancoding2009 <91250608+ryancoding2009@users.noreply.github.com>
2022-08-23 21:05:48 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+noseX=0;
+noseY=0;
+
+function preload() {
+    clown_nose = loadImage('https://i.postimg.cc/MG8Zn8bB/580b57fbd9996e24bc43bed5.png');
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.position(800, 300);
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialised');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    //fill(255,0,0);
+    //stroke(255,0,0);
+    //circle(noseX, noseY, 20);
+    image(clown_nose, noseX-15, noseY-10, 30, 30);
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
+
